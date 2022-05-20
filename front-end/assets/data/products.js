32adfc500cd337e85e1851ffa26d401b2293ce4c
StefanNel007 <stefan_nel@hotmail.com>
2022-05-20 09:14:53 +0200

    repository created with github

---
+++ front-end/assets/data/products.js
@@ -0,0 +1,121 @@
+const products = [
+  {
+    id: 0,
+    name: 'bear eraser',
+    href: '#',
+    price: 'R10',
+    description: 'bear eraser',
+    imageSrc: 'bearEraserImage',
+    imageAlt: 'bear eraser'
+  },
+  {
+    id: 1,
+    name: 'multicolor pens',
+    href: '#',
+    price: 'R20',
+    description: 'multicolor pens',
+    imageSrc: '',
+    imageAlt: 'multicolor pens'
+  },
+  {
+    id: 2,
+    name: 'puppy stickers (5)',
+    href: '#',
+    price: 'R2',
+    description: 'puppy stickers (5)',
+    imageSrc: '',
+    imageAlt: 'puppy stickers (5)'
+  },
+  {
+    id: 3,
+    name: 'animal cards',
+    href: '#',
+    price: 'R15',
+    description: 'animal cards',
+    imageSrc: '',
+    imageAlt: 'animal cards'
+  },
+  {
+    id: 4,
+    name: 'glitter paper',
+    href: '#',
+    price: 'R5',
+    description: 'glitter paper',
+    imageSrc: '',
+    imageAlt: 'glitter paper'
+  },
+  {
+    id: 5,
+    name: 'space stickers',
+    href: '#',
+    price: 'R2',
+    description: 'space stickers',
+    imageSrc: '',
+    imageAlt: 'space stickers'
+  },
+  {
+    id: 6,
+    name: 'slogan stickers',
+    href: '#',
+    price: 'R2',
+    description: 'slogan stickers',
+    imageSrc: '',
+    imageAlt: 'slogan stickers'
+  },
+  {
+    id: 7,
+    name: 'puppy pencil topper',
+    href: '#',
+    price: 'R10',
+    description: 'puppy pencil topper',
+    imageSrc: '',
+    imageAlt: 'puppy pencil topper'
+  },
+  {
+    id: 8,
+    name: 'food eraser',
+    href: '#',
+    price: 'R5',
+    description: 'food eraser',
+    imageSrc: '',
+    imageAlt: 'food eraser'
+  },
+  {
+    id: 9,
+    name: 'emoticon stickers',
+    href: '#',
+    price: 'R5',
+    description: 'emoticon stickers',
+    imageSrc: '',
+    imageAlt: 'emoticon stickers'
+  },
+  {
+    id: 10,
+    name: 'flower stickers',
+    href: '#',
+    price: 'R5',
+    description: 'flower stickers',
+    imageSrc: '',
+    imageAlt: 'flower stickers'
+  },
+  {
+    id: 11,
+    name: 'among us keyring',
+    href: '#',
+    price: 'R5',
+    description: 'among us keyring',
+    imageSrc: '',
+    imageAlt: 'among us keyring'
+  },
+  {
+    id: 12,
+    name: 'feather bookmark',
+    href: '#',
+    price: 'R5',
+    description: 'feather bookmark',
+    imageSrc: '',
+    imageAlt: 'feather bookmark'
+  }
+]
+
+export default products;
---
+++ front-end/pages/index.js
@@ -0,0 +1,78 @@
+import Head from 'next/head'
+import products from '../assets/data/products';
+import Image from 'next/image';
+
+import bearEraserImage from '../assets/images/bear-eraser.webp';
+import multicolorPensImage from '../assets/images/multicolor-pens.webp';
+import flowerStickerImage from '../assets/images/flower-sticker.webp';
+import amongUsKeychainImage from '../assets/images/among-us-keychain.webp';
+import emoticonStickersImage from '../assets/images/emoticon-stickers.webp';
+import foodEraserImage from '../assets/images/food-eraser.webp';
+import featherBookmarkImage from '../assets/images/feather-bookmark2.webp';
+
+
+const imageList = [ bearEraserImage, 
+                    multicolorPensImage, 
+                    bearEraserImage, 
+                    multicolorPensImage, 
+                    bearEraserImage, 
+                    multicolorPensImage, 
+                    bearEraserImage,
+                    multicolorPensImage, 
+                    foodEraserImage, 
+                    emoticonStickersImage, 
+                    flowerStickerImage, 
+                    amongUsKeychainImage, 
+                    featherBookmarkImage];
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>The Stationary Station - by Luke</title>
+        <meta name="description" content="The Stationary Station - Stationary shop for kids" />
+   
+      </Head>
+
+      <main>
+        <div className='flex flex-col items-center p-8 bg-cyan-300' >
+          <h1 
+            className='text-4xl font-bold uppercase'
+          >
+            the stationary station
+          </h1>
+          <h2 className='text-2xl'>by Luke</h2>
+        </div>
+
+        <div className="bg-white">
+      <div className="max-w-2xl px-4 py-16 mx-auto sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+        <h2 className="sr-only">Products</h2>
+
+        <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+          {products.map((product) => (
+            <a key={product.id} href={product.href} className="group">
+              <div className="w-full overflow-hidden bg-gray-200 rounded-lg aspect-w-1 aspect-h-1 xl:aspect-w-7 xl:aspect-h-8">
+                <Image
+                  src={imageList[product.id]}
+                  layout='fill'
+                  alt={product.imageAlt}
+                  className="object-cover object-center w-full h-full group-hover:opacity-75"
+                  priority
+                />
+              </div>
+              <h3 className="mt-4 text-gray-700 text-md">{product.name}</h3>
+              <p className="mt-1 text-lg font-medium text-gray-900">{product.price}</p>
+            </a>
+          ))}
+        </div>
+      </div>
+    </div>
+
+      </main>
+
+      <footer>
+
+      </footer>
+    </div>
+  )
+}
